6ca9fbaa65f269cee4117088cd3ef55616d05d06
santoshgmali <malisantosh1111@gmail.com>
2023-01-22 14:51:53 +0530

    Added file for Arraylist

---
+++ src/com/santosh/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.santosh;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		List list=new ArrayList();
+		list.add("Abc");
+		list.add("Xyz");
+	}
+
+}
